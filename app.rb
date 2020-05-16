a0bd042399998f0f187f6be8c542c1c80327c3a3
Zietieflr <lrmcguire93@gmail.com>
2020-05-15 18:04:14 -0600

    Completed lesson, scrubbed.

---
+++ app.rb
@@ -0,0 +1,8 @@
+require './lib/school.rb'
+
+kinard = School.new 'Kinard'
+
+kinard.add_student('Homer Simpson', 9)
+kinard.add_student('Jeff Baird', 10)
+kinard.add_student('Avi Flombaum', 10)
+kinard.add_student('Blake Johnson', 7)
